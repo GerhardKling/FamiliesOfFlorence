1c74de4490e98adbdc0d1398737752e0de1c679d
Gerhard Kling <67431049+GerhardKling@users.noreply.github.com>
2024-02-26 18:54:08 +0000

    Add files via upload

---
+++ StepOne/gui_1.py
@@ -0,0 +1,28 @@
+"""
+Builds GUI
+"""
+
+import tkinter as tk
+
+#Starts root
+root = tk.Tk()
+root.title('Families of Florence')
+
+#Size
+root.geometry("500x600")
+
+#Frames
+frm = tk.Frame(root)
+frm.grid(column = 0, row = 0)
+
+#Labels
+label_frm = tk.Label(frm, text = "Hello World!")
+label_frm.grid(column = 0, row = 0)
+
+#Buttons
+button_frm = tk.Button(frm, text = "Exit", command = root.destroy)
+button_frm.grid(column = 1, row = 0)
+
+#Mainloop at the end
+root.mainloop()
+
